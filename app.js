b1fb4a55fce96d944c5fc8c3265d2020fd3a41a2
Philip Purwoko <Philippurwoko123@gmail.com>
2020-11-13 09:18:34 +0700

    Menambahkan static css dan routing html files

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
+const rootDir = require('./util/path')
 
 const app = express();
 
@@ -7,8 +9,13 @@ const shopRoute = require('./routes/shop');
 const adminRoute = require('./routes/admin');
 
 app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.static(path.join(__dirname,'public')));
 
 app.use(shopRoute);
-app.use(adminRoute);
+app.use('/admin',adminRoute);
+
+app.use((req,res,next)=>{
+    res.status(404).sendFile(path.join(rootDir,'views','404.html'));
+});
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
+const rootDir = require('../util/path')
 
-router.get('/admin',(req,res,next)=>{
-    res.send('<h1>Admin</h1>');
+router.get('/add-product',(req,res,next)=>{
+    res.sendFile(path.join(rootDir,'views','add-product.html'));
+});
+
+router.post('/add-product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
 });
 
 module.exports = router;
