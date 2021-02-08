a57fc80b09e692d50c5ff83cf7d014035e17d85d
Philip Purwoko <philippurwoko123@gmail.com>
2021-02-08 12:55:57 +0700

    :beetle: Add token logic for user reset password

--- controllers/user.js
+++ controllers/user.js
@@ -38,15 +38,12 @@ exports.postLogout = (req, res) =>{
 }
 
 exports.getReset = (req, res)=>{
-    const successFlash = req.flash('success');
-    const error = req.flash('error');
-    const errorMessage = error.length > 0 ? error[0] : null;
-    const success = successFlash.length > 0 ? successFlash[0] : null;
+    const messageFLash = req.flash('message');
+    const message = messageFLash.length > 0 ? messageFLash[0] : null;
     res.render('user/reset', {
         'title':'Nusantaran JS | Reset Password',
         'path':'/reset',
-        'success': success,
-        'errorMessage': errorMessage
+        'message': message
     });
 }
 
@@ -56,41 +53,73 @@ exports.postReset = (req, res)=>{
             res.status(500).redirect('/500');
         } else {
             const token = buffer.toString('hex');
+            const email = {
+                to: req.body.email,
+                from: process.env.MAIL_SENDER,
+                subject: 'Nusantaran Reset User Password',
+                text: 'Reset password',
+                html: `<h2>Yoohoo</h2><p>Reset your password <a href="http://${process.env.IP_PUBLIC}/newpassword?email=${req.body.email}&token=${token}">here</a></p>`
+            };
             db('resettoken').returning('*').insert({
                 useremail: req.body.email,
                 token: token,
                 expired: Date.now() + 3600000
             }).then(()=>{
-                const email = {
-                    to: req.body.email,
-                    from: process.env.MAIL_SENDER,
-                    subject: 'Nusantaran Reset User Password',
-                    text: 'Reset password',
-                    html: `<h2>Yoohoo</h2><p>Reset your password <a href="http://${process.env.IP_PUBLIC}/newpassword?email=${req.body.email}&token=${token}">here</a></p>`
-                };
                 sgMail.send(email).then(()=>{
-                    req.flash('success', true);
+                    req.flash('message', 'Email has been sent, token is valid for one hour, please check your email');
                     res.status(200).redirect('/reset');
                 }).catch(()=>{
                     res.status(500).redirect('/500');
                 })
             }).catch(()=>{
-                req.flash('success', false);
-                req.flash('error', 'Email already sent, valid for one hour');
-                res.status(200).redirect('/reset');
+                db('resettoken').where('useremail', req.body.email).update({
+                    token: token,
+                    expired: Date.now() + 3600000
+                }).then(()=>{
+                    sgMail.send(email).then(()=>{
+                        req.flash('message', 'Expiration token has been updated to 1 hour, please check your email');
+                        res.status(200).redirect('/reset');
+                    }).catch(()=>{
+                        res.status(500).redirect('/500');
+                    })
+                });
             });
         }
     });
 }
 
 exports.getNewPassword = (req, res)=>{
-    const email = req.query.email
+    const email = req.query.email;
     const token = req.query.token;
-    res.render('user/new-password', {
-        'title':'Nusantaran JS | Set New Password',
-        'path':'/newpassword',
-        'token': token,
-        'email': email
+    db('resettoken').where('useremail', email).then((users)=>{
+        if (users.length === 0){
+            throw new Error('User not found');
+        } else {
+            const user = users[0];
+            let error = null;
+            if (token == user.token){
+                if (user.expired < Date.now()){
+                    error = 'Sorry, your token is expired';
+                }
+            } else {
+                error = 'Wrong token for this user';
+            }
+            res.render('user/new-password', {
+                'title':'Nusantaran JS | Set New Password',
+                'path':'/newpassword',
+                'token': token,
+                'email': email,
+                'error': error
+            });
+        }
+    }).catch(()=>{
+        res.render('user/new-password', {
+            'title':'Nusantaran JS | Set New Password',
+            'path':'/newpassword',
+            'token': token,
+            'email': email,
+            'error': 'Sorry, the user you are looking for is not found'
+        });
     });
 }
 
