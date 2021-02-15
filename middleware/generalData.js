36d59ca88502e72c41976452d367ceefbec1b83c
Philip Purwoko <philippurwoko123@gmail.com>
2021-02-15 14:51:28 +0700

    :tulip: Styling CSS Card detail, card list, cart page

--- middleware/generalData.js
+++ middleware/generalData.js
@@ -1,6 +1,7 @@
 module.exports = (req, res, next)=>{
     res.locals.isAuthenticated = req.session.isAuthenticated;
     res.locals.isAdmin = req.session.isAdmin;
+    res.locals.username = req.session.user.name
     res.locals.csrfToken = req.csrfToken();
     next();
 }
