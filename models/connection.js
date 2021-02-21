5b39bc6957ee0e574ea103954792a4bfbeddf13b
Philip Purwoko <philippurwoko123@gmail.com>
2021-02-21 08:38:52 +0700

    :shield: Add sanitizion protection to update image path file

--- models/connection.js
+++ models/connection.js
@@ -1,6 +1,5 @@
 module.exports = {
     host: process.env.HOST,
-    port: process.env.DB_PORT,
     user: process.env.USER,
     password: process.env.PASSWORD,
     database: process.env.DATABASE_NAME
