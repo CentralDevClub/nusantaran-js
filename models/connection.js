f8d36c3becaf8945d70ffe09ebc6a5338ea60822
PhilipPurwoko <philippurwoko123@gmail.com>
2021-02-21 08:12:30 +0800

    :milky_way: Add connection port to database server

--- models/connection.js
+++ models/connection.js
@@ -1,5 +1,6 @@
 module.exports = {
     host: process.env.HOST,
+    port: process.env.DB_PORT,
     user: process.env.USER,
     password: process.env.PASSWORD,
     database: process.env.DATABASE_NAME
