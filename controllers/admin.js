8251e9d1358817ea761f24731aec7a40a5054a92
Philip Purwoko <philippurwoko123@gmail.com>
2021-04-28 21:23:28 +0700

    :beetle: Finished fix error middleware in controllers

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,50 +1,50 @@
-const chalk = require('chalk');
-const sanitize = require('sanitize-filename');
-const Product = require('../models/products');
-const User = require('../models/users');
-const { validationResult } = require('express-validator');
-const fs = require('fs');
-const itemPerPage = 10;
+const chalk = require('chalk')
+const sanitize = require('sanitize-filename')
+const Product = require('../models/products')
+const User = require('../models/users')
+const { validationResult } = require('express-validator')
+const fs = require('fs')
+const itemPerPage = 10
 
 
-const sanitizeImage = (oldPath)=>{
-    return 'images/' + sanitize(oldPath.split('\\')[1]);
+const sanitizeImage = (oldPath) => {
+    return 'images/' + sanitize(oldPath.split('\\')[1])
 }
 
-exports.getAllUsers = (_req, res) => {
-    User.allUser().then((users)=>{
+exports.getAllUsers = (_req, res, next) => {
+    User.allUser().then((users) => {
         const hasUser = users.length > 0 ? true : false
         res.render('admin/users', {
-            'title':'Nusantaran JS | Manage Users',
-            'path':'/users',
+            'title': 'Nusantaran JS | Manage Users',
+            'path': '/users',
             'hasUser': hasUser,
             'users': users,
         })
-    }).catch(()=>{
-        res.status(500).redirect('/500');
+    }).catch((err) => {
+        next(err)
     })
 }
 
-exports.getProduct = (req,res)=>{
-    const page = req.query.page ? req.query.page -1 : 0;
+exports.getProduct = (req, res, next) => {
+    const page = req.query.page ? req.query.page - 1 : 0
     Product.fetchChunk(page, itemPerPage).then((productAndLength) => {
-        const products = productAndLength.chunkData;
-        const length = productAndLength.tableRowsCount;
-        const totalPage = Math.ceil(length / itemPerPage);
-        const placeholder = req.flash('placeholder');
+        const products = productAndLength.chunkData
+        const length = productAndLength.tableRowsCount
+        const totalPage = Math.ceil(length / itemPerPage)
+        const placeholder = req.flash('placeholder')
         const placeholderData = placeholder.length > 0 ? placeholder[0] : {}
-        const error = req.flash('errorMessage');
+        const error = req.flash('errorMessage')
         const errorMessage = error.length > 0 ? error[0] : null
-        const hasProduct = products.length > 0 ? true : false;
-        const displayPage = totalPage * itemPerPage >= itemPerPage ? true : false;
+        const hasProduct = products.length > 0 ? true : false
+        const displayPage = totalPage * itemPerPage >= itemPerPage ? true : false
         const limit = {
             firstPage: 1,
             lastPage: totalPage
         }
 
-        res.render('admin/product',{
-            'title':'Nusantaran JS | Admin Products',
-            'path':'/product',
+        res.render('admin/product', {
+            'title': 'Nusantaran JS | Admin Products',
+            'path': '/product',
             'hasProduct': hasProduct,
             'products': products,
             'errorMessage': errorMessage,
@@ -54,17 +54,17 @@ exports.getProduct = (req,res)=>{
             'totalPage': totalPage,
             'displayPage': displayPage,
             'limit': limit
-        });
-    }).catch((error)=>{
-        console.log(error);
-        res.redirect('/500');
-    });
-};
+        })
+    }).catch((error) => {
+        console.log(error)
+        next(error)
+    })
+}
 
-exports.postAddProduct = (req, res)=>{
-    const validationError = validationResult(req);
-    if (validationError.isEmpty()){
-        const safeImage = sanitizeImage(req.file.path);
+exports.postAddProduct = (req, res) => {
+    const validationError = validationResult(req)
+    if (validationError.isEmpty()) {
+        const safeImage = sanitizeImage(req.file.path)
         const product = new Product(
             req.body.name,
             req.body.category,
@@ -72,75 +72,75 @@ exports.postAddProduct = (req, res)=>{
             req.body.price,
             safeImage,
             req.session.user.email
-        );
+        )
         product.save().then((product) => {
-            console.log(chalk.blue(`Product added : ${product[0].name}`));
-            res.redirect('/admin/product');
-        }).catch(()=>{
-            console.log(chalk.red('Product name is already have'));
-            req.flash('errorMessage', [`Product "${req.body.name}" is already in database`]);
-            req.flash('errors', [{param:'name'}]);
+            console.log(chalk.blue(`Product added : ${product[0].name}`))
+            res.redirect('/admin/product')
+        }).catch(() => {
+            console.log(chalk.red('Product name is already have'))
+            req.flash('errorMessage', [`Product "${req.body.name}" is already in database`])
+            req.flash('errors', [{ param: 'name' }])
             req.flash('placeholder', {
                 'name': req.body.name,
                 'category': req.body.category,
                 'description': req.body.description,
                 'price': req.body.price
             })
-            res.status(422).redirect('/admin/product');
-        });
+            res.status(422).redirect('/admin/product')
+        })
     } else {
         try {
-            fs.unlinkSync(sanitizeImage(req.file.path));
-        } catch (error){
-            console.log(error);
-        };
-        req.flash('errorMessage', validationError.array()[0].msg);
-        req.flash('errors', validationError.array());
+            fs.unlinkSync(sanitizeImage(req.file.path))
+        } catch (error) {
+            console.log(error)
+        }
+        req.flash('errorMessage', validationError.array()[0].msg)
+        req.flash('errors', validationError.array())
         req.flash('placeholder', {
             'name': req.body.name,
             'category': req.body.category,
             'description': req.body.description,
             'price': req.body.price
-        });
-        res.redirect('/admin/product');
+        })
+        res.redirect('/admin/product')
     }
-};
+}
 
-exports.postUpdateProduct = (req, res)=>{
-    let product = req.body;
+exports.postUpdateProduct = (req, res, next) => {
+    let product = req.body
     product.imagepath = sanitizeImage(product.imagepath)
-    product.image = req.file ? sanitizeImage(req.file.path) : product.imagepath;
-    if (req.file){
+    product.image = req.file ? sanitizeImage(req.file.path) : product.imagepath
+    if (req.file) {
         try {
-            fs.unlinkSync(product.imagepath);
-        } catch(err){
-            console.log(err);
+            fs.unlinkSync(product.imagepath)
+        } catch (err) {
+            console.log(err)
         }
     }
-    Product.updateProduct(product).then(()=> {
-        res.redirect('/admin/product');
-    }).catch((error)=>{
-        console.log(error);
-        res.redirect('/500');
-    });
-};
+    Product.updateProduct(product).then(() => {
+        res.redirect('/admin/product')
+    }).catch((error) => {
+        console.log(error)
+        next(error)
+    })
+}
 
-exports.postDeleteProduct = (req,res)=>{
-    Product.deleteProduct(req.body.id).then(()=>{
-        res.redirect('/admin/product');
-    }).catch((error)=>{
-        console.log(error);
-        res.redirect('/500');
-    });
-};
+exports.postDeleteProduct = (req, res, next) => {
+    Product.deleteProduct(req.body.id).then(() => {
+        res.redirect('/admin/product')
+    }).catch((error) => {
+        console.log(error)
+        next(error)
+    })
+}
 
-exports.postChangeStatus = (req, res)=>{
-    const id = req.body.id;
-    const status = req.body.status;
-    User.updateStatus(id, status).then(()=>{
-        res.status(200).redirect('/myorder');
-    }).catch((err)=>{
-        console.log(err);
-        res.status(500).redirect('/500');
+exports.postChangeStatus = (req, res, next) => {
+    const id = req.body.id
+    const status = req.body.status
+    User.updateStatus(id, status).then(() => {
+        res.status(200).redirect('/myorder')
+    }).catch((err) => {
+        console.log(err)
+        next(err)
     })
 }
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,38 +1,38 @@
-require('dotenv').config;
-const Users = require('../models/users');
-const knex = require('knex');
-const db_config = require('../models/db-config').config;
-const db = knex(db_config);
-const chalk = require('chalk');
-const bcrypt = require('bcrypt');
-const crypto = require('crypto');
-const { validationResult } = require('express-validator');
-const Email = require('../emails/emails');
+require('dotenv').config
+const Users = require('../models/users')
+const knex = require('knex')
+const db_config = require('../models/db-config').config
+const db = knex(db_config)
+const chalk = require('chalk')
+const bcrypt = require('bcrypt')
+const crypto = require('crypto')
+const { validationResult } = require('express-validator')
+const Email = require('../emails/emails')
 const sgMail = require('@sendgrid/mail')
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
 
 exports.getRegister = (req, res) => {
-    const error = req.flash('errorMessage');
-    const message = error.length > 0 ? error[0] : null;
+    const error = req.flash('errorMessage')
+    const message = error.length > 0 ? error[0] : null
     res.render('auth/register', {
-        'title':'Nusantaran JS | Register',
-        'path':'/register',
+        'title': 'Nusantaran JS | Register',
+        'path': '/register',
         'errorMessage': message,
-        'errors':[],
-        'placeholder':{
+        'errors': [],
+        'placeholder': {
             'name': null,
-            'email':null,
-            'address':null
+            'email': null,
+            'address': null
         }
-    });
+    })
 }
 
-exports.getLogin = (req, res)=>{
-    const success = req.flash('successMessage');
-    const successMessage = success.length > 0 ? success[0] : null;
-    const error = req.flash('errorMessage');
-    const message = error.length > 0 ? error[0] : null;
+exports.getLogin = (req, res) => {
+    const success = req.flash('successMessage')
+    const successMessage = success.length > 0 ? success[0] : null
+    const error = req.flash('errorMessage')
+    const message = error.length > 0 ? error[0] : null
     res.render('auth/login', {
         'title': 'Nusantaran JS | Login',
         'path': '/login',
@@ -43,28 +43,28 @@ exports.getLogin = (req, res)=>{
     })
 }
 
-exports.postRegister = (req, res)=>{
-    const validationError = validationResult(req);
-    if (!validationError.isEmpty()){
+exports.postRegister = (req, res, next) => {
+    const validationError = validationResult(req)
+    if (!validationError.isEmpty()) {
         return res.status(422).render('auth/register', {
-            'title':'Nusantaran JS | Register',
-            'path':'/register',
+            'title': 'Nusantaran JS | Register',
+            'path': '/register',
             'errorMessage': validationError.array()[0].msg,
             'errors': validationError.array(),
-            'placeholder':{
+            'placeholder': {
                 'name': req.body.name,
                 'email': req.body.email,
-                'address':req.body.address
+                'address': req.body.address
             }
-        });
+        })
     }
 
     Users.addUser(req.body.name, req.file.path, req.body.address, req.body.email, req.body.password).then(() => {
-        crypto.randomBytes(32, (error, buffer)=>{
-            if (error){
-                res.status(500).redirect('/500');
+        crypto.randomBytes(32, (error, buffer) => {
+            if (error) {
+                next(error)
             } else {
-                const token = buffer.toString('hex');
+                const token = buffer.toString('hex')
                 const email = {
                     to: req.body.email,
                     from: process.env.MAIL_SENDER,
@@ -76,61 +76,61 @@ exports.postRegister = (req, res)=>{
                         'buttonLink': `http://${process.env.IP_PUBLIC}/verified?email=${req.body.email}&token=${token}`,
                         'buttonText': 'Verify Account'
                     })
-                };
+                }
 
                 db('verifytoken').returning('*').insert({
                     email: req.body.email,
                     token: token,
                     expired: Date.now() + 3600000
-                }).then(()=>{
-                    sgMail.send(email).then(()=>{
-                        console.log(chalk.green(`Email Sent to ${req.body.email}`));
-                        req.flash('successMessage', 'Successfully signed up. Please check your email and verify your account');
-                        res.redirect('/login');
-                    }).catch(err=>{
-                        console.log(chalk.red(err));
-                        res.status(500).redirect('/500');
-                    });
-                }).catch(()=>{
+                }).then(() => {
+                    sgMail.send(email).then(() => {
+                        console.log(chalk.green(`Email Sent to ${req.body.email}`))
+                        req.flash('successMessage', 'Successfully signed up. Please check your email and verify your account')
+                        res.redirect('/login')
+                    }).catch(err => {
+                        console.log(chalk.red(err))
+                        next(err)
+                    })
+                }).catch(() => {
                     db('verifytoken').where('email', req.body.email).update({
                         token: token,
                         expired: Date.now() + 3600000
-                    }).then(()=>{
-                        sgMail.send(email).then(()=>{
-                            req.flash('successMessage', 'Expiration token has been updated to 1 hour, please check your email');
-                            res.status(200).redirect('/login');
-                        }).catch(()=>{
-                            res.status(500).redirect('/500');
+                    }).then(() => {
+                        sgMail.send(email).then(() => {
+                            req.flash('successMessage', 'Expiration token has been updated to 1 hour, please check your email')
+                            res.status(200).redirect('/login')
+                        }).catch((err) => {
+                            next(err)
                         })
-                    }).catch((err)=>{
-                        console.log(err);
-                        res.status(500).redirect('/500');
-                    });
-                });
+                    }).catch((err) => {
+                        console.log(err)
+                        next(err)
+                    })
+                })
             }
-        });
-    }).catch((err)=>{
-        console.log(err);
-        db('users').select('*').where('email', req.body.email).then((users)=>{
-            const user = users[0];
-            if (user.verified === true){
+        })
+    }).catch((err) => {
+        console.log(err)
+        db('users').select('*').where('email', req.body.email).then((users) => {
+            const user = users[0]
+            if (user.verified === true) {
                 return res.status(422).render('auth/register', {
-                    'title':'Nusantaran JS | Register',
-                    'path':'/register',
+                    'title': 'Nusantaran JS | Register',
+                    'path': '/register',
                     'errorMessage': 'Email already used, try another one',
-                    'errors': [{'param':'email'}],
-                    'placeholder':{
+                    'errors': [{ 'param': 'email' }],
+                    'placeholder': {
                         'name': req.body.name,
                         'email': req.body.email,
-                        'address':req.body.address
+                        'address': req.body.address
                     }
-                });
+                })
             } else {
-                crypto.randomBytes(32, (error, buffer)=>{
-                    if (error){
-                        res.status(500).redirect('/500');
+                crypto.randomBytes(32, (error, buffer) => {
+                    if (error) {
+                        next(error)
                     } else {
-                        const token = buffer.toString('hex');
+                        const token = buffer.toString('hex')
                         const email = {
                             to: req.body.email,
                             from: process.env.MAIL_SENDER,
@@ -142,115 +142,116 @@ exports.postRegister = (req, res)=>{
                                 'buttonLink': `http://${process.env.IP_PUBLIC}/verified?email=${req.body.email}&token=${token}`,
                                 'buttonText': 'Verify Account'
                             })
-                        };
+                        }
                         db('verifytoken').where('email', req.body.email).update({
                             token: token,
                             expired: Date.now() + 3600000
-                        }).then(()=>{
-                            sgMail.send(email).then(()=>{
-                                req.flash('successMessage', 'Expiration token has been updated to 1 hour, please check your email');
-                                res.status(200).redirect('/login');
-                            }).catch(()=>{
-                                res.status(500).redirect('/500');
+                        }).then(() => {
+                            sgMail.send(email).then(() => {
+                                req.flash('successMessage', 'Expiration token has been updated to 1 hour, please check your email')
+                                res.status(200).redirect('/login')
+                            }).catch((err) => {
+                                console.log(err)
+                                next(err)
                             })
-                        });
+                        })
                     }
-                });
+                })
             }
-        }).catch((err)=>{
-            console.log(err);
-            res.status(500).redirect('/500');
-        });
-    });
+        }).catch((err) => {
+            console.log(err)
+            next(err)
+        })
+    })
 }
 
-exports.getVerified = (req, res)=>{
-    const email = req.query.email;
-    const token = req.query.token;
-    db('verifytoken').where('email', email).then((users)=>{
-        if (users.length === 0){
-            throw new Error('User not found');
+exports.getVerified = (req, res, next) => {
+    const email = req.query.email
+    const token = req.query.token
+    db('verifytoken').where('email', email).then((users) => {
+        if (users.length === 0) {
+            throw new Error('User not found')
         } else {
-            const user = users[0];
-            let error = null;
-            if (token == user.token){
-                if (user.expired < Date.now()){
-                    error = 'Sorry, your token is expired';
+            const user = users[0]
+            let error = null
+            if (token == user.token) {
+                if (user.expired < Date.now()) {
+                    error = 'Sorry, your token is expired'
                 }
             } else {
-                error = 'Wrong token for this user';
+                error = 'Wrong token for this user'
             }
 
-            Users.verifyAccount(email).then(()=>{
+            Users.verifyAccount(email).then(() => {
                 res.render('auth/verified', {
-                    'title':'Nusantaran JS | Verify Account',
-                    'path':'/verified',
+                    'title': 'Nusantaran JS | Verify Account',
+                    'path': '/verified',
                     'error': error
-                });
-            }).catch((err)=>{
-                console.log(err);
-                res.status(500).redirect('/500');
-            });
+                })
+            }).catch((err) => {
+                console.log(err)
+                next(err)
+            })
         }
-    }).catch(()=>{
+    }).catch(() => {
         res.render('user/new-password', {
-            'title':'Nusantaran JS | Set New Password',
-            'path':'/newpassword',
+            'title': 'Nusantaran JS | Set New Password',
+            'path': '/newpassword',
             'token': token,
             'email': email,
             'error': 'Sorry, the user you are looking for is not found'
-        });
-    });
+        })
+    })
 }
 
-exports.postLogin = (req, res)=>{
-    Users.findUserByEmail(req.body.email).then((users)=>{
+exports.postLogin = (req, res, next) => {
+    Users.findUserByEmail(req.body.email).then((users) => {
         try {
             const user = users[0]
-            console.log(chalk.blue(`User found ${user.email}`));
-            if (user.verified == true){
-                bcrypt.compare(req.body.password, user.password).then((success)=>{
-                    if (success){
-                        console.log(chalk.green(`Successfully logged in - ${req.body.email}`));
-                        req.session.user = user;
-                        req.session.isAuthenticated = true;
-                        db('administrator').select('*').then((admins)=>{
-                            if (admins){
-                                for (const admin of admins){
-                                    if (req.body.email === admin.email){
-                                        req.session.isAdmin = true;
+            console.log(chalk.blue(`User found ${user.email}`))
+            if (user.verified == true) {
+                bcrypt.compare(req.body.password, user.password).then((success) => {
+                    if (success) {
+                        console.log(chalk.green(`Successfully logged in - ${req.body.email}`))
+                        req.session.user = user
+                        req.session.isAuthenticated = true
+                        db('administrator').select('*').then((admins) => {
+                            if (admins) {
+                                for (const admin of admins) {
+                                    if (req.body.email === admin.email) {
+                                        req.session.isAdmin = true
                                     }
                                 }
                             }
                             res.redirect('/')
-                        }).catch(()=>{
-                            console.log('Catch at : models/auth.js:110');
-                            res.redirect('/500');
-                        });
+                        }).catch((err) => {
+                            console.log('Catch at : models/auth.js:110')
+                            next(err)
+                        })
                     } else {
-                        throw new Error(`Wrong password for "${user.email}"`);
+                        throw new Error(`Wrong password for "${user.email}"`)
                     }
-                }).catch((error)=>{
-                    console.log(chalk.red(error));
-                    req.flash('errorMessage', 'Wrong password for this user');
-                    req.flash('placeholder', req.body.email);
-                    req.flash('errors', {'param':'password'});
-                    res.redirect('/login');
+                }).catch((error) => {
+                    console.log(chalk.red(error))
+                    req.flash('errorMessage', 'Wrong password for this user')
+                    req.flash('placeholder', req.body.email)
+                    req.flash('errors', { 'param': 'password' })
+                    res.redirect('/login')
                 })
             } else {
-                req.flash('errorMessage', 'User is not verified. Check your email and verify your account');
-                req.flash('placeholder', req.body.email);
-                req.flash('errors', {'param':'email'});
-                res.redirect('/login');
+                req.flash('errorMessage', 'User is not verified. Check your email and verify your account')
+                req.flash('placeholder', req.body.email)
+                req.flash('errors', { 'param': 'email' })
+                res.redirect('/login')
             }
         } catch (error) {
-            throw new Error(error);
+            throw new Error(error)
         }
-    }).catch(()=>{
-        console.log(chalk.red('User not found'));
-        req.flash('errorMessage', 'User not found. Please register');
-        req.flash('placeholder', req.body.email);
-        req.flash('errors', {'param':'email'});
-        res.redirect('/login');
-    });
+    }).catch(() => {
+        console.log(chalk.red('User not found'))
+        req.flash('errorMessage', 'User not found. Please register')
+        req.flash('placeholder', req.body.email)
+        req.flash('errors', { 'param': 'email' })
+        res.redirect('/login')
+    })
 }
